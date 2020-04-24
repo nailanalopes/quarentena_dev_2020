85b5d914fdbbe87cd8d38886db7929c3561ee795
nailanalopes <62913829+nailanalopes@users.noreply.github.com>
2020-04-23 21:23:50 -0300

    Calculadora Idade
    Tentativa de melhorar a ficha de academia de uma outra atividade no #QuarentenaDev

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-function gerar(){
-  contador=1000000
-    while(contador>0){
-        contador=contador-1
-        htmldocertificado=emitirCertificado(inputNome.value)
-  }
-  listalink=[
-  "https://codepen.io/NayLopes/pen/RwWPKMV",
-  "https://codepen.io/NayLopes/pen/dyYPaVE",
-  "https://codepen.io/NayLopes/pen/ExVaOVB",
-  "https://codepen.io/NayLopes/pen/gOapmXQ",
-  "https://codepen.io/NayLopes/pen/WNQvVvY",
-  "https://codepen.io/NayLopes/pen/eYpJMqb",
-  "https://codepen.io/NayLopes/pen/KKdVRbL"
-  ]
+function calcular(){
   
-
-  contalink=0
-  listalinkcomhtml=""
-  while(contalink<7){
-   
-  link=listalink[contalink]
-  listalinkcomhtml=listalinkcomhtml+`<li><a href="${link}">${link}</a></li>`
-  contalink=contalink+1
-}
-
-document.body.innerHTML=`
-${htmldocertificado}
-  <ol>
-  ${listalinkcomhtml}
-  </ol>`
-
+var data = new Date()
+  
+  anoatual=data.getFullYear();
+  datadenascimento=inputNascimento.value.split("/")
+  idade=anoatual-datadenascimento[2]
+    botaoCalcular.innerHTML="Idade: "+ idade
 }
